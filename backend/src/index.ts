837589f468a2daf945c69ed7ac997caeccc9c0ed
absirmulugeta <absirmulugeta@gmail.com>
2025-11-06 22:13:58 +0300

    change for expiratinon time

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -47,7 +47,7 @@ app.use(helmet());
 
 // Rate Limiting
 const limiter = rateLimit({
-    windowMs: 15 * 60 * 1000, // 15 minutes
+    windowMs: 58 * 60 * 1000, // 15 minutes
     max: 100, // Limit each IP to 100 requests per windowMs
     message: 'Too many requests from this IP, please try again after 15 minutes'
 });
